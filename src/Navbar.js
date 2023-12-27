8898e606b1897c01aad2fe2e55f6b4f4415d11ac
Josh L <lloydrobotics@gmail.com>
2023-12-27 20:44:24 +0000

    react tut 10 - outputting lists

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,19 +4,14 @@ const Navbar = () => {
       <h1>The Dojo Blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a
-          href="/create"
-          style={{
-            color: "white",
-            backgroundColor: "#f1356d",
-            borderRadius: "8px",
-          }}
-        >
-          New Blog
-        </a>
+        <a href="/create" style={{
+          color: "white",
+          backgroundColor: "#f1356d",
+          borderRadius: "8px",
+          }}>New Blog</a>
       </div>
     </nav>
   );
-};
+}
 
 export default Navbar;
