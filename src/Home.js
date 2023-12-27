015348384500529d3e26d721c6b7715bb61747cc
Josh L <lloydrobotics@gmail.com>
2023-12-27 15:29:36 +0000

    react tutorial 8 - useState (hooks)

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [name, setName] = useState("Mario");
+  const [age, setAge] = useState(25);
+
+  const handleClick = () => {
+    setName("Luigi");
+    setAge(30);
+  };
+
   return (
     <div className="home">
       <h1>Homepage</h1>
+      <p>
+        {name} is {age} years old!
+      </p>
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 };
