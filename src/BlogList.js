b512c46cc1ff8090c83ea24348f528ca6bcc111e
Josh L <lloydrobotics@gmail.com>
2023-12-28 06:58:01 +0000

    lesson 12 - reusing components

--- src/BlogList.js
+++ src/BlogList.js
@@ -3,7 +3,8 @@ const BlogList = ( {blogs, title} ) => {
 //   const title = props.title;
 
   return (
-    <div className="home">
+    <div className="blog-list">
+        <h2>{ title }</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
